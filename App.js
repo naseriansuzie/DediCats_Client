0d28d806c2ab9fe78ae35735be1e23e678a24289
Daeyeon Kim <danielkim@16KimD.local>
2020-02-04 00:09:51 +0900

    Temp test code for react-navigation test

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import Signin from './src/pages/Signin';
+import Signup from './src/pages/Signup';
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: Signin,
+  },
+  Second: {
+    screen: Signup,
+  },
+},
+{
+  initialRouteName: 'Home',
+});
+
+const AppContainer = createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
   container: {
@@ -8,16 +25,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
   },
 });
 
 export default function App() {
   return (
     // <Provider>
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Signin />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    <AppContainer />
+    // {/* </View> */}
     // </Provider>
   );
 }
