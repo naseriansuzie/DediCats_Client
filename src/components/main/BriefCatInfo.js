7c412d0548e57719053735c5358a6de446a25f67
Daeyeon Kim <danielkim@16KimD.local>
2020-02-15 03:37:22 +0900

    Fix styles of components in MainMap & BriefCatInfo

--- src/components/main/BriefCatInfo.js
+++ src/components/main/BriefCatInfo.js
@@ -3,11 +3,14 @@ import {
   StyleSheet,
   Text,
   View,
-  Button,
   Image,
+  TouchableOpacity,
+  Dimensions,
 } from 'react-native';
 import { withNavigation } from 'react-navigation';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+// import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
@@ -15,46 +18,69 @@ const styles = StyleSheet.create({
   },
   card: {
     backgroundColor: '#ececec',
-    height: 250,
-    width: 320,
-    padding: 24,
+    height: height * 0.3,
+    width: width * 0.9,
+    paddingHorizontal: 15,
+    paddingVertical: 5,
     borderRadius: 24,
   },
   cardImg: {
     height: 120,
-    width: 200,
+    width: 120,
     alignItems: 'center',
+    marginVertical: 10,
+    borderRadius: 24,
   },
   cardtitle: {
-    fontSize: 12,
-    marginTop: 5,
+    fontSize: 20,
     fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: -110,
+    marginBottom: 20,
+    marginRight: 20,
   },
   cardDescription: {
     fontSize: 12,
     color: '#444',
+    textAlign: 'right',
+  },
+  hideCarouselBtn: {
+    flexDirection: 'row-reverse',
+    alignItems: 'flex-end',
+    top: 15,
+    right: 15,
+    width: 350,
+  },
+  catInfoBtn: {
+    marginTop: 130,
+    alignItems: 'center',
   },
 });
+
 
 class BriefCatInfo extends React.Component {
   render() {
     return (
       <View style={styles.card}>
         <TouchableOpacity
+          style={styles.hideCarouselBtn}
           onPress={() => this.props.hideCarousel()}
         >
           <Text>X</Text>
         </TouchableOpacity>
-        <Image
-          style={styles.cardImg}
-          source={this.props.item.img}
-        />
-        <Text style={styles.cardtitle}>{this.props.item.name}</Text>
-        <Text style={styles.cardDescription}>{this.props.item.content}</Text>
+        <View>
+          <Image
+            style={styles.cardImg}
+            source={this.props.item.img}
+          />
+          <Text style={styles.cardtitle}>{this.props.item.name}</Text>
+          <Text style={styles.cardDescription}>{this.props.item.content}</Text>
+        </View>
         <TouchableOpacity
+          style={styles.catInfoBtn}
           onPress={() => this.props.navigation.navigate('CatInfo')}
         >
-          <Text>Move to Cat Info</Text>
+          <Text style={{ fontSize: 15 }}>고양이 정보 보러가기</Text>
         </TouchableOpacity>
       </View>
     );
