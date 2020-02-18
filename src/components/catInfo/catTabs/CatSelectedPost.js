50d7fc20d528ef6e26f84306b48de96a6a8ad421
Sooji Kim <yh0gogo@gmail.com>
2020-02-18 22:39:00 +0900

    Add up abusing report post function

--- src/components/catInfo/catTabs/CatSelectedPost.js
+++ src/components/catInfo/catTabs/CatSelectedPost.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
 import { inject, observer } from 'mobx-react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Modal,
+  Alert,
+} from 'react-native';
 import {
   Container,
   Header,
@@ -12,18 +20,52 @@ import {
   Body,
   List,
 } from 'native-base';
+import { AntDesign } from '@expo/vector-icons';
 import CatComment from './CatComment';
 
-const defaultPhotoUrl = [
-  'https://ca.slack-edge.com/T5K7P28NN-U5NKFNELV-g3d11e3cb933-512',
-  'https://ca.slack-edge.com/T5K7P28NN-UFMJV5U03-g8dbe796546d-512',
-][Math.floor(Math.random() * 2)];
+const defaultPhotoUrl =
+  'https://ca.slack-edge.com/T5K7P28NN-UFMJV5U03-g8dbe796546d-512';
 
 const styles = StyleSheet.create({
   container: {
     flex: 3,
     width: '100%',
   },
+  cardView: {
+    flex: 1,
+    borderWidth: 10,
+  },
+  reportView: { alignItems: 'flex-end' },
+  ellipsis: { fontSize: 24 },
+  modalView: {
+    flex: 1,
+    alignItems: 'flex-end',
+    marginRight: 2,
+    marginTop: 93,
+  },
+  reportBox: {
+    backgroundColor: '#edf1f5',
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    height: '10%',
+    width: '60%',
+    paddingTop: 5,
+    paddingLeft: 5,
+    marginRight: 10,
+  },
+  warningIcon: {
+    fontSize: 17,
+  },
+  reportBtn: {
+    backgroundColor: '#f38847',
+    marginVertical: 10,
+    marginLeft: 5,
+    marginRight: 200,
+    paddingVertical: 5,
+    borderRadius: 5,
+  },
+  reportTxt: { color: 'white', textAlign: 'center' },
   userImg: {
     borderRadius: 10,
     height: 50,
@@ -41,8 +83,8 @@ const styles = StyleSheet.create({
 
 class CatSelectedPost extends React.Component {
   componentDidMount() {
-    console.log('selectedCatPost = ', this.props.selectedCatPost);
     console.log('CatSelectedPost mount');
+    console.log('selectedCatPost = ', this.props.selectedCatPost);
   }
 
   render() {
@@ -50,6 +92,9 @@ class CatSelectedPost extends React.Component {
       selectedCatPost,
       selectedCatCommentList,
       convertDateTime,
+      canReportPost,
+      setCanReportPost,
+      reportPost,
     } = this.props;
 
     const usrImgUri =
@@ -61,7 +106,71 @@ class CatSelectedPost extends React.Component {
       <Container style={styles.container}>
         <Header style={{ display: 'none' }} />
         <Content>
-          <Card style={styles.flex1}>
+          <Card style={styles.cardView}>
+            <View style={styles.reportView}>
+              <TouchableOpacity
+                onPress={() => {
+                  setCanReportPost(true);
+                  console.log(canReportPost);
+                }}
+              >
+                <AntDesign name="ellipsis1" style={styles.ellipsis} />
+              </TouchableOpacity>
+            </View>
+            <Modal
+              transparent
+              visible={canReportPost}
+              onRequestClose={() => {
+                setCanReportPost(false);
+              }}
+            >
+              <View style={styles.modalView}>
+                <View>
+                  <TouchableOpacity
+                    onPress={() => {
+                      setCanReportPost(false);
+                      console.log(canReportPost);
+                    }}
+                  >
+                    <AntDesign name="ellipsis1" style={styles.ellipsis} />
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.reportBox}>
+                  <Text style={styles.paddingVertical5}>
+                    <AntDesign name="warning" style={styles.warningIcon} />{' '}
+                    게시글 신고
+                  </Text>
+                  <Text note style={styles.paddingVertical5}>
+                    이 게시글에 부적절한 내용이 있습니다.
+                  </Text>
+                  <View style={styles.reportBtn}>
+                    <TouchableOpacity
+                      onPress={async () => {
+                        const result = await reportPost();
+                        if (result) {
+                          Alert.alert(
+                            '신고 완료',
+                            '해당 신고 요청이 관리자에게 접수되었습니다.',
+                            [
+                              {
+                                text: '확인',
+                                onPress: () => setCanReportPost(false),
+                              },
+                            ],
+                          );
+                        }
+                      }}
+                    >
+                      <Text style={styles.reportTxt}>신고</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+                <TouchableOpacity
+                  style={{ width: '100%', height: '100%' }}
+                  onPress={() => setCanReportPost(false)}
+                />
+              </View>
+            </Modal>
             <CardItem style={styles.flex1}>
               <Left>
                 <Thumbnail
@@ -113,8 +222,11 @@ class CatSelectedPost extends React.Component {
   }
 }
 
-export default inject(({ cat, helper }) => ({
+export default inject(({ cat, helper, report }) => ({
   selectedCatPost: cat.selectedCatPost,
   selectedCatCommentList: cat.selectedCatCommentList,
   convertDateTime: helper.convertDateTime,
+  canReportPost: report.canReportPost,
+  setCanReportPost: report.setCanReportPost,
+  reportPost: report.reportPost,
 }))(observer(CatSelectedPost));
