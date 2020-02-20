1f6ad26a9ab3ec562e4bc7809ff08214927422fd
Sooji Kim <yh0gogo@gmail.com>
2020-02-20 23:08:04 +0900

    Move default cat image from MyPage to MyCat

--- src/components/my/MyCat.js
+++ src/components/my/MyCat.js
@@ -10,13 +10,15 @@ const styles = StyleSheet.create({
 
 const defaultPhotoUrl =
   'https://scontent-gmp1-1.xx.fbcdn.net/v/t1.0-9/35362350_2128812463813083_109129312195575808_o.jpg?_nc_cat=100&_nc_ohc=UfkbqZH71McAX849PZo&_nc_ht=scontent-gmp1-1.xx&oh=e8d961f7d73f856eb7c5e9c42dfa4259&oe=5EF62D35';
+const DEFAULT_CAT =
+  'https://www.pngitem.com/pimgs/m/85-850345_dog-puppy-silhouette-svg-png-icon-free-download.png';
 
-const MyCat = ({ idx, catPhoto, address, catNickname }) => (
+const MyCat = ({ catPhoto, address, catNickname }) => (
   <ListItem thumbnail>
     <Left>
       <Thumbnail
         square
-        source={{ uri: catPhoto || defaultPhotoUrl }}
+        source={{ uri: catPhoto || DEFAULT_CAT }}
         style={styles.radius}
       />
     </Left>
