077791fa3a5adfafaad811552a72defe668a223e
Daeyeon Kim <danielkim@16KimD.local>
2020-02-22 14:51:06 +0900

    Create & save observables & actions from CatStore

---
+++ src/stores/CommentStore.js
@@ -0,0 +1,227 @@
+import { observable, action, decorate } from 'mobx';
+import axios from 'axios';
+import { SERVER_URL } from 'react-native-dotenv';
+import socketio from 'socket.io-client';
+import { Alert } from 'react-native';
+
+const defaultCredential = { withCredentials: true };
+class CommentStore {
+  constructor(root) {
+    this.root = root;
+  }
+
+  // post에 해당하는 commentList
+  commentList = [];
+
+  // 수정, 삭제, 신고를 위해 선택한 comment
+  selectedComment = null;
+
+  // 작성하는 댓글
+  inputComment = '';
+
+  // comment를 수정하는 중인지 상태 - true면 수정 중
+  commentModifyState = false;
+
+  // * 소켓 아이디
+  socketId = null;
+
+  //! 소켓 연결 여부
+  isConnectSocket = false;
+
+  //! 추가된 댓글
+  newComment = null;
+
+  //! 댓글 페이지
+  commentPage = 0;
+
+  // 처음 방에 입장했을 때 댓글 수
+  initialComments = 0;
+
+  // socket 연결 시작
+  connectSocket = () => {
+    const socket = socketio.connect(`${SERVER_URL}`, {
+      query: `postId=${this.root.post.selectedCatPost.id}`,
+    });
+    const helper = sockets => {
+      sockets.emit('new comment', 'hello');
+
+      sockets.on('connect', () => {
+        if (socket.connected) {
+          this.socketId = sockets.id;
+          this.isConnectSocket = true;
+        } else {
+          console.log('Connection Failed');
+        }
+      });
+
+      sockets.on('drop', () => {
+        console.log('drop');
+        sockets.disconnect();
+        this.isConnectSocket = false;
+      });
+
+      sockets.on('new comment', comment => {
+        this.newComment = comment;
+        this.commentList.unshift(this.newComment);
+      });
+    };
+    helper(socket);
+  };
+
+  // 소켓 연결해제
+  offUser = async navigation => {
+    const result = await axios
+      .get(
+        `${SERVER_URL}/post/disconnect/?socket_id=${this.socketId}`,
+        defaultCredential,
+      )
+      .then(res => true)
+      .catch(err => {
+        this.root.auth.expiredTokenHandler(err, navigation);
+        console.dir(err);
+      });
+    return result;
+  };
+
+  // 선택한 포스트 기준으로 댓글 리스트를 받아오는 함수
+  getCommentList = async navigation => {
+    try {
+      const postId = this.root.post.selectedCatPost.id;
+      const url = `${SERVER_URL}/comment/${postId}/${this.commentPage}`;
+      const comment = await axios.get(url);
+      if (comment) {
+        console.log('서버에서 받은 코멘트들', comment.data.length);
+        this.commentList = this.commentList.concat(
+          comment.data,
+        );
+        console.log('받아온 코멘트 리스트', this.commentList.length);
+      }
+      return;
+    } catch (error) {
+      this.root.auth.expiredTokenHandler(err, navigation);
+      console.error(error);
+    }
+  };
+
+  // 댓글 창에서 나갈 때 리셋
+  resetCommentState = () => {
+    this.commentList = [];
+    this.selectedComment = null;
+    this.commentPage = 0;
+    this.newComment = null;
+    this.initialComments = 0;
+  };
+
+  // 예전 댓글 로드
+  _handleLoadMoreComments = async navigation => {
+    this.commentPage += 1;
+    await this.getCommentList(navigation);
+  };
+
+  // * 댓글 추가와 수정 둘다 가능
+  addComment = (mode, navigation) => {
+    const url =
+      mode === 'new'
+        ? `${SERVER_URL}/comment/add`
+        : `${SERVER_URL}/comment/update`;
+
+    const postId = this.root.post.selectedCatPost.id;
+
+    if (mode === 'new') {
+      const newCommentInfo = { postId, content: this.inputComment };
+      return axios
+        .post(url, newCommentInfo, defaultCredential)
+        .then(res => {
+          this.initialComments += 1;
+          this.root.helper.clearInput('cat', 'inputComment');
+          return res;
+        })
+        .catch(err => {
+          if (err.response && err.response.status === 409) {
+            Alert.alert('댓글 업로드에 실패했습니다. 다시 한 번 등록해주세요!');
+          } else {
+            this.root.auth.expiredTokenHandler(err, navigation);
+            console.dir(err);
+          }
+        });
+    }
+    const updateCommentInfo = {
+      commentId: this.selectedComment.id,
+      content: this.inputComment,
+    };
+    return axios
+      .post(url, updateCommentInfo, defaultCredential)
+      .then(res => {
+        this.root.helper.clearInput('cat', 'inputComment');
+        return res.data;
+      })
+      .catch(err => {
+        if (err.response && err.response.status === 409) {
+          Alert.alert('댓글 수정에 실패했습니다. 다시 한 번 등록해주세요!');
+        } else console.dir(err);
+      });
+  };
+
+  // 댓글을 state에 할당
+  setCatComment = async comment => {
+    this.selectedComment = comment;
+    return new Promise((resolve, reject) => {
+      resolve(true);
+    });
+  };
+
+  // 수정 상황 불린 변경
+  setCommentModify = () => {
+    this.commentModifyState = !this.commentModifyState;
+  };
+
+  // 댓글 수정
+  modifyComment = comment => {
+    this.setCatComment(comment);
+    this.setCommentModify();
+    this.inputComment = comment.content;
+  };
+
+  // 댓글 삭제
+  deleteComment = async (comment, navigation) => {
+    await this.setCatComment(comment);
+    axios
+      .post(
+        `${SERVER_URL}/comment/delete`,
+        { commentId: this.selectedComment.id },
+        defaultCredential,
+      )
+      .then(res => {
+        this.initialComments -= 1;
+        Alert.alert('게시글이 삭제되었습니다.');
+      })
+      .catch(err => {
+        this.root.auth.expiredTokenHandler(err, navigation);
+        console.dir(err);
+      });
+  };
+}
+
+decorate(CommentStore, {
+  commentList: observable,
+  selectedComment: observable,
+  inputComment: observable,
+  commentModifyState: observable,
+  socketId: observable,
+  isConnectSocket: observable,
+  newComment: observable,
+  commentPage: observable,
+  initialComments: observable,
+  connectSocket: action,
+  offUser: action,
+  getCommentList: action,
+  resetCommentState: action,
+  _handleLoadMoreComments: action,
+  addComment: action,
+  setCatComment: action,
+  setCommentModify: action,
+  modifyComment: action,
+  deleteComment: action,
+});
+
+export default CommentStore;
