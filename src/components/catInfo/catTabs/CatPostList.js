200d2139b098208195a5de315599a23670422249
Daeyeon Kim <danielkim@16KimD.local>
2020-03-01 02:40:26 +0900

    fix syntax error

--- src/components/catInfo/catTabs/CatPostList.js
+++ src/components/catInfo/catTabs/CatPostList.js
@@ -29,10 +29,10 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 50,
     alignItems: 'center',
   },
-  keyboard: { 
-      width: '95%',
-     position:"absolute",
-     zIndex: 1,
+  keyboard: {
+    width: '95%',
+    position: 'absolute',
+    zIndex: 1,
   },
   safeArea: {
     flex: 3,
@@ -102,7 +102,7 @@ class CatPostList extends React.Component {
       isRefreshingPost,
       _handleRefresh,
     } = this.props;
-    const { loadingFont } = this.state;
+    const { loadingFont, visibility } = this.state;
     if (loadingFont) {
       this.loadFont();
       return <View />;
@@ -111,9 +111,9 @@ class CatPostList extends React.Component {
       <View style={styles.container}>
         <View style={styles.radiusView}>
           <SafeAreaView style={styles.safeArea}>
-          <KeyboardAvoidingView style={styles.keyboard}>
-            {this.state.visibility ? <CatPostInput /> : null}
-          </KeyboardAvoidingView>
+            <KeyboardAvoidingView style={styles.keyboard}>
+              {visibility ? <CatPostInput /> : null}
+            </KeyboardAvoidingView>
             <FlatList
               data={postList}
               renderItem={this._renderItem}
